2b329063e560540f4676ca1236cb1c1b97bb298f
SANADASHRAF <76807340+SANADASHRAF@users.noreply.github.com>
2022-10-22 00:29:48 +0200

    add category and it's filter

--- src/app/componants/home/home.component.ts
+++ src/app/componants/home/home.component.ts
@@ -11,12 +11,18 @@ export class HomeComponent implements OnInit {
   
 
   products:any[] = [];
+  categories:any[]=[];
+
   constructor(private service:ProductService) { }
 
   ngOnInit(): void {
     this.getProducts();
+    this.getAllCategory();
   }
 
+// call serveses
+
+//allproduct 
   getProducts() {  
     this.service.getAllProducts().subscribe((res:any) => {
   
@@ -25,5 +31,63 @@ export class HomeComponent implements OnInit {
      errpr=>{alert(errpr.massege)}
      )
   }
+
+
  
-}
+  // category 
+  getAllCategory(){
+      this.service.getAllCategory().subscribe(
+      (res:any)=>{ this.categories=res; }, 
+       error   =>{alert(error)}
+       )}
+
+  //method of change event of select category 
+   selectcategory(event:any){
+    
+        let selectcategory=event.target.value;
+
+        if(selectcategory=="all")
+        {
+          this.getProducts();
+        }
+        else
+        {
+        this.gitFilteredCategory(selectcategory);
+        }
+       }
+
+
+    gitFilteredCategory(value:string){
+      this.service.gitFilteredCategory(value).subscribe(
+        (res:any)=>{this.products=res},
+         error    =>{alert(error)}
+      )
+
+       }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
